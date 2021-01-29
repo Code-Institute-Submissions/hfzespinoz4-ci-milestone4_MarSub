c932c09b740c86df4893121c11017f9e90ee1b6e
Harvin Zúniga <harvin.zuniga@gmail.com>
2021-01-29 09:36:47 +0000

    Added BTT button

--- services/views.py
+++ services/views.py
@@ -1,27 +1,37 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Services
+from .models import Services, Category
 
 
 # A view to render the services page.
 def all_services(request):
     services = Services.objects.all()
     query = None
+    categories = None
 
+    # Search functionallity,
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['Category'].split(',')
+            services = services.filter(category__category__in=categories)
+            categories = Category.objects.filter(category__in=categories)
+
+        # Search field functionallity
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "Please enter a search criteria")
                 return redirect(reverse('services'))
 
-            queries = Q(service__icontains=query) | Q(description__icontains=query)
+            # Filtering from Service and Categories Models
+            queries = Q(service__icontains=query) | Q(description__icontains=query) | Q(short_description__icontains=query)
             services = services.filter(queries)
 
     context = {
         'services': services,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'services/services.html', context)
